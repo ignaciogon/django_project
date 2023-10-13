286d9917e1182181b43e2e846223e4b4bd3d4e5e
ignaciogon <ignaciogon.g@gmail.com>
2023-10-13 08:59:07 -0500

    adding user registration functionality

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Iggy',
-        'title': 'Blog post #1',
-        'content': 'First post content',
-        'date_posted': 'October 11, 2023'
-    },
-    {
-        'author': 'Naty',
-        'title': 'Blog post #2',
-        'content': 'Second post content',
-        'date_posted': 'October 12, 2023'
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
